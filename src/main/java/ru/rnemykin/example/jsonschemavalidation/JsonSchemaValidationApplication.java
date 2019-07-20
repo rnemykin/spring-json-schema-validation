41525b04a5d790c589d8fe53653edacbb1f2e7a1
rnemykin <nemykinroman@gmail.com>
2019-07-20 12:41:59 +0300

    base config

---
+++ src/main/java/ru/rnemykin/example/jsonschemavalidation/JsonSchemaValidationApplication.java
@@ -0,0 +1,13 @@
+package ru.rnemykin.example.jsonschemavalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonSchemaValidationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsonSchemaValidationApplication.class, args);
+    }
+
+}
