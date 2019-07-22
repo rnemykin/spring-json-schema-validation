0288f30260cdf005f0f47571d01e6c7e3c876aca
rnemykin <nemykinroman@gmail.com>
2019-07-23 00:04:23 +0300

    main logic

--- src/main/java/ru/rnemykin/example/jsonschemavalidation/JsonSchemaValidationApplication.java
+++ src/main/java/ru/rnemykin/example/jsonschemavalidation/JsonSchemaValidationApplication.java
@@ -1,10 +1,62 @@
 package ru.rnemykin.example.jsonschemavalidation;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.github.fge.jackson.JsonLoader;
+import com.github.fge.jsonschema.core.report.ProcessingMessage;
+import com.github.fge.jsonschema.core.report.ProcessingReport;
+import com.github.fge.jsonschema.main.JsonSchemaFactory;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.io.Resource;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import ru.rnemykin.example.jsonschemavalidation.model.enums.PaymentMethod;
+import ru.rnemykin.example.jsonschemavalidation.repository.PaymentMethodRepository;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+@RestController
 @SpringBootApplication
+@RequiredArgsConstructor
 public class JsonSchemaValidationApplication {
+    private final JsonSchemaFactory jsonSchemaFactory;
+    private final PaymentMethodRepository paymentMethodRepository;
+
+    @Value("classpath:/jsonschema/Order.json")
+    private Resource orderJsonSchemaResource;
+
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void onApplicationReady() {
+        paymentMethodRepository.saveAll(
+                Stream.of("Visa", "MasterCard", "MIR", "PayPal")
+                        .map(PaymentMethod::valueOf)
+                        .collect(Collectors.toList())
+        );
+    }
+
+
+    @SneakyThrows
+    @PostMapping("/orders/validate")
+    public List<String> validate(@RequestBody JsonNode order) {
+        JsonNode schema = JsonLoader.fromResource("/jsonschema/Order.json");
+        ProcessingReport report = jsonSchemaFactory.getValidator().validate(schema, order, true);
+        return StreamSupport.stream(report.spliterator(), false)
+                .map(ProcessingMessage::asJson)
+                .filter(m -> "validation".equals(m.get("domain").asText()))
+                .map(m -> m.get("message").asText())
+                .collect(Collectors.toList());
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(JsonSchemaValidationApplication.class, args);
--- src/test/java/ru/rnemykin/example/jsonschemavalidation/JsonSchemaValidationApplicationTests.java
+++ src/test/java/ru/rnemykin/example/jsonschemavalidation/JsonSchemaValidationApplicationTests.java
@@ -1,16 +1,45 @@
 package ru.rnemykin.example.jsonschemavalidation;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
-@RunWith(SpringRunner.class)
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @SpringBootTest
+@AutoConfigureMockMvc
+@RunWith(SpringRunner.class)
 public class JsonSchemaValidationApplicationTests {
 
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private ObjectMapper mapper;
+
+
     @Test
-    public void contextLoads() {
+    public void orderValidation() throws Exception {
+        MockHttpServletResponse response = mvc.perform(
+                post("/orders/validate")
+                        .content("{\"seller\": \"Nike\"}")
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isOk()).andReturn().getResponse();
+
+        List errorMessages = mapper.readValue(response.getContentAsByteArray(), List.class);
+        Assert.assertTrue("must contains elements", !errorMessages.isEmpty());
+        System.out.println(errorMessages);
     }
 
 }
